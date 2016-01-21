68a1aefedad977b766d68e6871f1682b2f4d2680
awronka <alexiusmichael@gmail.com>
2016-01-21 18:14:08 -0500

    drop down doesnt work

--- public/js/main.js
+++ public/js/main.js
@@ -1,68 +1,55 @@
-
 document.addEventListener('DOMContentLoaded', function(){
-console.log('loaded')
-var gameBody = document.createElement('div')
-gameBody.style.width ='600px';
-gameBody.style.height = '600px';
-gameBody.style.border = '1px dotted black'
-gameBody.style.left = "0"
-gameBody.style.right = '0'
-gameBody.style.margin = '20px auto'
-document.body.appendChild(gameBody)
 
-var bird = document.createElement('div')
-bird.classList.add('bird')
+var webPage = document.getElementById('container');
 
-gameBody.appendChild(bird)
-console.log(bird.style)
-bird.style.marginTop = '200px';
-bird.style.marginLeft = '250px';
+// create header with all components
+// ------------------------------------------------
+
+var header = document.createElement('div')
+header.classList.add('navbar')
+header.classList.add('navbar-default')
+header.classList.add('navbar-fixed-top')
+header.classList.add('navbar-color-fix')
+
+webPage.appendChild(header)
+console.log(header.style)
+
+var tabBar = document.createElement('div')
+tabBar.classList.add('nav')
+tabBar.classList.add('tabBar')
 
 
-// setInterval(function(){
-// 	console.log('hit')
-// 	bird.style.marginTop = (parseInt(bird.style.marginTop, 10) - 1)+'px'
-// },20)
+header.appendChild(tabBar)
 
-document.body.addEventListener('keypress', function(e){
-		if(e.which ===32){
-			clearInterval(jumpInterval)
-			var initPos = bird.style.marginTop.split('p')
-			var jumpTarget = (Number(initPos[0]) - 1)-100
-			var velocity = 1;
-			var jumpStart = true;
-			var jumpOrigin = Number(initPos[0])
-			var jumpInterval = setInterval(function(){
-				var position = bird.style.marginTop.split('p')
-				if(jumpStart){
-					if(jumpTarget <=(Number(position[0], 10))-50){
-					bird.style.marginTop = (Number(position[0]) - 2)+'px'
-					}
-					else if(jumpTarget <=(Number(position[0]))-25){
-					bird.style.marginTop = (Number(position[0]) - 1)+'px'
-					}
-					else if(jumpTarget <(Number(position[0]))){
-					bird.style.marginTop = (Number(position[0]) - .5)+'px'
-					}
-					else if(jumpTarget === (Number(position[0]))){jumpStart = false
-						bird.style.marginTop = (Number(position[0]))+'px'
-					}
-				}
-				else if(!jumpStart){
-					if(jumpOrigin >=(Number(position[0]))){
-					bird.style.marginTop = (Number(position[0]) + .5)+'px'
-					}
-					else if(jumpOrigin >=(Number(position[0]))+10){
-					bird.style.marginTop = (Number(position[0]) + 1)+'px'
-					}
-					else{
-					velocity++;
-					bird.style.marginTop = (Number(position[0]) + 2)+'px'
-					}
-				}	
-			}, 0)
-		}
-})
+var tabContents =[['tab','tab','tab','tab','tab']]
 
+var tabTitles = ['about', 'get data', 'tools']
+var tabObjects = []
+for(var i =0; i<tabTitles.length; i++){
+	var tabBox = document.createElement('div')
+	tabBox.innerHTML = "<div class='miniTab'><span>"+tabTitles[i]+"<div></span>";
+	tabBox.classList.add('tabBox')
+	console.log(tabBox)
+	tabBar.appendChild(tabBox)
+
+	tabBox.addEventListener('mouseover', function(){
+			var start = document.createElement('div')
+				start.classList.add('first-appear-tab')
+				start.innerHTML = "<div class='miniTab'><span>"+tabContents[0][0]+" "+ 0 +"<div></span>";
+				this.appendChild(start)
+
+			for(var k = 0; k<tabContents.length; k++){
+				var list = document.createElement('div')
+				list.classList.add('appearTab')
+				list.innerHTML = "<div class='miniTab'><span>"+tabContents[0][k]+" "+ k +"<div></span>";
+				this.appendChild(list)
+			}
+	})
+	tabObjects.push(tabBox)
+
+}
+
+console.log(tabObjects)
 
 })
+
